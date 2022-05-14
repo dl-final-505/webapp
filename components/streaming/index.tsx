acdd0c7c2503daa72dca482a28372c55e22b322a
Idan Shoshana <ishoshana@microsoft.com>
2022-05-14 17:48:30 +0300

    Update streaming component

--- components/streaming/index.tsx
+++ components/streaming/index.tsx
@@ -1,13 +1,41 @@
-import { LegacyRef, useEffect, useRef } from "react";
-import { LogEntry as LogEntryModel } from "../../models";
+import { useEffect, useRef } from "react";
 import styles from "../../styles/Home.module.css";
-import LogEntry from "../logEntry";
 
-interface Props {
-}
+let recorder: MediaRecorder = null;
 
 const Streaming = ({}: Props) => {
   const video = useRef<HTMLVideoElement>(null);
+  const recordedChunks = useRef([]);
+
+
+  const handleDataAvailable = (event: BlobEvent) => {
+    if (event.data.size > 0) {
+      recordedChunks.current = [event.data];
+      download();
+    }
+  }
+
+  function download() {
+    var blob = new Blob(recordedChunks.current, {
+      type: "video/webm",
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    a.href = url;
+    a.download = "test.webm";
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+
+  const recordVideo = (stream: MediaStream) => {
+    const options = { mimeType: "video/webm; codecs=vp9" };
+    recorder = new MediaRecorder(stream, options);
+    recorder.ondataavailable = handleDataAvailable;
+    recorder.start();
+  }
 
   useEffect(() => {
     if (navigator.mediaDevices.getUserMedia) {
@@ -16,12 +44,25 @@ const Streaming = ({}: Props) => {
         .then(function (stream) {
           if (video.current) {
             video.current.srcObject = stream;
+            recordVideo(stream);
           }
         })
         .catch(function (err0r) {
           console.log("Something went wrong!");
         });
     }
+
+    const stopAndStart = () => {
+      if (recorder?.state === "recording") {
+        recorder?.stop();
+        recorder?.start();
+        setTimeout(stopAndStart, 4000);        
+      }
+    }
+
+    setTimeout((event) => {
+      stopAndStart();
+    }, 9000);
   }, []);
 
   return (
